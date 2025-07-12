107ff0bd6da4517c67f53f74ebaa7a433343616f
mouzarthy <mouzarthy2007@hotmail.com>
2025-07-11 21:48:49 -0300

    L27 servo

--- pwm.v
+++ pwm.v
@@ -7,5 +7,40 @@ module PWM (
 );
 
 //insira o seu código aqui
+
+reg [31:0] counter;
+
+always @( posedge clk or negedge rst_n ) 
+begin
+    if( !rst_n ) 
+    begin
+        // Reset assíncrono
+        counter <= 0;
+        pwm_out <= 0;
+    end 
+    else 
+    begin
+        // Incrementa o contador
+        if( counter <= period ) 
+        begin
+            counter <= counter + 1;
+        end 
+        else 
+        begin
+            // Reinicia o contador quando atinge o período
+            counter <= 0;
+        end
+
+        // Gera a saída PWM
+        if( counter < duty_cycle ) 
+        begin
+            pwm_out <= 1;
+        end 
+        else 
+        begin
+            pwm_out <= 0;
+        end
+    end
+end
     
 endmodule
--- servo.v
+++ servo.v
@@ -9,4 +9,52 @@ module servo #(
 
 //insira seu código aqui
 
+localparam integer MIN_PULSE_CYCLES = PERIOD * 5 / 100; // 1ms (25MHz -> 25000 ciclos)
+localparam integer MAX_PULSE_CYCLES = PERIOD * 10 / 100;  // 2ms (25MHz -> 50000 ciclos)
+localparam integer WAIT             = CLK_FREQ * 5;
+
+reg [31:0]  duty_cycle  = MIN_PULSE_CYCLES;
+reg [31:0]  delay       = 0;
+reg         dir         = 0;
+
+always @( posedge clk or negedge rst_n ) 
+begin
+    if( !rst_n ) 
+    begin
+        duty_cycle  <= MIN_PULSE_CYCLES;
+        delay       <= 0;
+        dir         <= 0;
+    end 
+    else 
+    begin
+        if( delay >= WAIT )
+        begin
+            delay <= 0;
+            
+            if( dir == 0 )
+            begin
+                duty_cycle <= MAX_PULSE_CYCLES;
+            end
+            else
+            begin
+                duty_cycle <= MIN_PULSE_CYCLES;
+            end
+            
+            dir <= -dir;
+        end
+        else
+        begin
+            delay <= delay + 1;
+        end
+    end
+end
+
+PWM pwm0    (
+            .clk( clk ),
+            .rst_n( rst_n ),
+            .duty_cycle( duty_cycle ),
+            .period( PERIOD ),
+            .pwm_out( servo_out )
+            );
+
 endmodule
